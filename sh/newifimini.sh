abe78222462d841f9573d03646326c383ea2c167
jmqyl <82797863+jmqyl@users.noreply.github.com>
2024-05-18 23:18:26 +0800

    Update newifimini.sh

--- sh/newifimini.sh
+++ sh/newifimini.sh
@@ -73,6 +73,16 @@ sed -i 's/必须是 IPv4 地址/IPv4 地址或域名/g' feeds/luci/applications/
 rm -rf feeds/luci/applications/luci-app-serverchan
 cp -af feeds/fichenx/luci-app-serverchan feeds/luci/applications/luci-app-serverchan
 
+#luci-app-msd_lite
+rm -rf feeds/luci/applications/luci-app-msd_lite
+#cp -af feeds/jmqyl/luci-app-serverchan feeds/luci/applications/luci-app-serverchan
+git clone https://github.com/jarod360/luci-app-msd_lite feeds/luci/applications/luci-app-msd_lite
+
+#luci-app-xupnpd
+rm -rf feeds/luci/applications/luci-app-xupnpd
+#cp -af feeds/jmqyl/luci-app-serverchan feeds/luci/applications/luci-app-serverchan
+git clone https://github.com/jarod360/luci-app-xupnpd feeds/luci/applications/luci-app-xupnpd
+
 #修改默认主题
 sed -i 's|set luci.main.mediaurlbase|#set luci.main.mediaurlbase|g' feeds/luci/themes/luci-theme-argon/root/etc/uci-defaults/30_luci-theme-argon
 sed -i 's|set luci.main.mediaurlbase|#set luci.main.mediaurlbase|g' feeds/luci/themes/luci-theme-argon-mod/root/etc/uci-defaults/90_luci-theme-argon
